b263c12eead816b8ff2c513f78554d3948f4ff3d
Thomas Viking Christiansson <160872481+ThomasCsson@users.noreply.github.com>
2024-04-11 14:58:48 +0200

    Update test.py

--- test.py
+++ test.py
@@ -3,6 +3,7 @@ from rdkit.Chem import Draw
 
 mol_smi = input('SMILES: ')
 mol = Chem.MolFromSmiles(mol_smi)
-img = Draw.MolToImage(mol)
+molH = Chem.AddHs(mol)
+img = Draw.MolToImage(molH)
 img.show()
 
