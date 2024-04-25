ee5913db22f2b39348c26fd148fb0c3b78307b0f
Igor Gonteri <igor.gonteri@epfl.ch>
2024-04-25 15:22:58 +0200

    add of pandas lecture

---
+++ Thomas/testigor.py
@@ -0,0 +1,33 @@
+
+import pandas as pd
+
+# Lecture du fichier texte
+with open('abundance.txt', 'r', encoding='ISO-8859-1') as file:
+    # Sauter la première ligne
+    next(file)
+    lines = file.readlines()
+
+# Initialisation des listes pour stocker les valeurs des colonnes
+colonne1 = []
+colonne2 = []
+colonne3 = []
+
+
+# Parcours des lignes et extraction des valeurs
+for line in lines:
+    values = line.split()
+
+    colonne1.append(values[0])
+    colonne2.append(float(values[1]))
+    colonne3.append(float(values[2]))
+
+data = {
+    'Atom': colonne1,
+    'Mass': colonne2,
+    'Abundance %': pd.to_numeric(colonne3)
+}
+
+# Création du DataFrame
+DF = pd.DataFrame(data)
+
+print(DF)
