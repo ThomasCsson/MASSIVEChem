e3dea0a6843961a02ad91d82fc5eae5037c9d918
Thomas Viking Christiansson <160872481+ThomasCsson@users.noreply.github.com>
2024-04-26 15:22:45 +0200

    Update Dont_touch_thomas.py

--- Thomas/Dont_touch_thomas.py
+++ Thomas/Dont_touch_thomas.py
@@ -12,21 +12,36 @@ df = pd.read_csv('/Users/thomaschristiansson/Documents/GitHub/ppchem-project-Chr
                  , header=None
                  , names=['Atom', 'Mass', 'Percentage'])
 
+
 #mass = [mass1, mass2, mass3,...]
+
 mass = df['Mass'].tolist()
+
+
 #change from elements (not sure) to floats
+
 mass = [float(m) for m in mass]
 
+
 #abundance = [ab1, ab2, ab3,...]
+
 abundance_percent = df['Percentage'].tolist()
+
+
 #change from elements (not sure) to floats
+
 abundance_percent = [float(ap) for ap in abundance_percent]
+
+
 #from percent to proba
+
 abundance = []
 for percent in abundance_percent:
     abundance.append(percent/100)
 
+
 #isotopes = [iso1, iso2, iso3,...]
+
 isotopes = df['Atom'].tolist()
 
 abundance = []
@@ -42,8 +57,11 @@ mol_smi = input('Enter SMILEs: ')
 mol_without_Hs = Chem.MolFromSmiles(mol_smi)
 mol = Chem.AddHs(mol_without_Hs)
 
+
 #Timing element (not very useful but its pretty so it's staying)
+
 start_time = time.time()
+
 
 #Function that takes in list of atoms and then gives a list of list of shape [[mass,proba],[mass,proba],...]
 
@@ -74,19 +92,25 @@ def main_function (mol):
 
     while len(list_atoms)>0:
 
+
         #This runs over all atoms in molecule
+
         list_output_new = []
 
         for i in range (len(list_output)):
 
+
             #This makes us run over all lists in list obtained before
+
             mass_copy = mass.copy()
             abundance_copy = abundance.copy()
             isotopes_copy = isotopes.copy() 
 
             for _ in range (isotopes.count(list_atoms[0])):
 
+
                 #This for-loop runs over all isotope types of the atom type in pos 0 in list_atoms (input list)
+
                 index = isotopes.index(list_atoms[0])
                 new_mass = list_output[i][0] + mass_copy[index]
                 new_proba = list_output[i][1] * abundance_copy[index]
@@ -97,7 +121,10 @@ def main_function (mol):
                 
         list_output = list_output_new
         list_atoms.pop(0)
+
+
     #Conversion of list_output (which is a list of lists) to a combination of two lists (x_axis & y_axis)
+
     x_axis, y_axis = [],[]
     x_axis_final, y_axis_final = [],[]
     for j in range (len(list_output)):
@@ -106,6 +133,7 @@ def main_function (mol):
     
 
     #Compression of lists x_axis & y_axis into x_axis_final & y_axis_final so that peaks corresponding to same mass will be represented together 
+    
     for j in range (len(x_axis)):
             if x_axis.count(x_axis[j]) == 1:
                 x_axis_final.append(x_axis[j])
@@ -119,11 +147,16 @@ def main_function (mol):
                 y_axis_final[index] =y_axis_final[index] + y_axis[j]
     
     
-    print(len(list_output))
-    print(len(x_axis_final))
-    print(len(y_axis_final))
-    print(max(y_axis))
-    print(max(y_axis_final))
+    max_x = max(x_axis_final)
+    min_x = max(x_axis_final)
+
+    
+
+    #test
+
+    
+    plt.plot(x_axis_final,y_axis_final)
+    plt.show()
     return x_axis_final,y_axis_final
 
 print(main_function(mol))
