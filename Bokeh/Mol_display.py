799b48268495c15058d1664f2b189e08492ca300
Igor Gonteri <igor.gonteri@epfl.ch>
2024-04-29 16:23:26 +0200

    upd

--- Bokeh/Mol_display.py
+++ Bokeh/Mol_display.py
@@ -1,41 +1,57 @@
-from rdkit import Chem
+
 from bokeh.plotting import figure, show
-import numpy as np
-from rdkit.Chem import Draw
+from rdkit.Chem import Draw, AllChem
 from rdkit import Chem
-from PIL import Image
-from io import BytesIO
+from bokeh.plotting import row
 
-
-
-def validate_smiles(smiles):
-    mol = Chem.MolFromSmiles(smiles)
-    if mol is None:
-        print("Invalid SMILES input.")
-        exit()
-    return True
 
 input_mol = input('SMILES: ')
 
-
-mol = Chem.MolFromSmiles(input_mol)
-
-image = Draw.MolToImage(mol)
-image.show()
-
-
 # Function to generate RDKit molecule image and save to file
-def save_molecule_image_to_file(mol, file_path):
+def save_molecule_image_to_file(smi, file_path, show_Hs=False, show_3D = False):
     # Generate the image from the molecule
-    image = Chem.Draw.MolToImage(mol)
+    mol = Chem.MolFromSmiles(smi)
+
+    # Adds the hydrogens to the molecule if specified
+    if show_Hs:
+        mol = Chem.AddHs(mol)
+
+    # Show the molecule in 3D if specified
+    if show_3D:
+        mol_3D = AllChem.EmbedMolecule(mol)
+
+    image = Draw.MolToImage(mol)
 
     # Save the image to a file
     image.save(file_path)
 
-if mol is None:
-    print("Invalid SMILES input.")
-    exit()
-
 output_file_path = "molecule_image.png"
-save_molecule_image_to_file(mol, output_file_path)
+save_molecule_image_to_file(input_mol, output_file_path, False, False)
+
+def mol_web_show(image_url):
+
+    # Creating a Bokeh figure
+    p = figure(width=400, height=400,toolbar_location=None, x_range=(0, 1), y_range=(0, 1))
+    p.image_url(url=[image_url], x=0, y=1, w=1, h=1)
+
+    # Hide grid lines and axes
+    p.xgrid.grid_line_color = None
+    p.ygrid.grid_line_color = None
+    p.xaxis.visible = False
+    p.yaxis.visible = False
+
+    return p
+
+
+
+image_url = "/Users/igorgonteri/Documents/GitHub/ppchem-project-Christiansson-Gonteri-Humery/Bokeh/molecule_image.png"
+
+show(mol_web_show(image_url))
+
+def final_layout(p1, p2):
+    layout = row(p1,p2)
+    show(layout)
+
+
+
 
