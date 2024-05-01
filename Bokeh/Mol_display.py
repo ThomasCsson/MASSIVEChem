633749714fbd4173a233c0665602e2513cb3604e
Igor Gonteri <igor.gonteri@epfl.ch>
2024-05-01 22:40:12 +0200

    update

--- Bokeh/Mol_display.py
+++ Bokeh/Mol_display.py
@@ -48,32 +48,126 @@ def mol_web_show(image_url):
 
 image_url = "/Users/igorgonteri/Documents/GitHub/ppchem-project-Christiansson-Gonteri-Humery/Bokeh/molecule_image.png"
 
-show(mol_web_show(image_url))
+#show(mol_web_show(image_url))
 
 def final_layout(p1, p2):
     layout = row(p1,p2)
     show(layout)
 
+input_mol = input('SMILEs : ')
 
-def subgroup_value (mol_smi, dict_functional_groups):
+functional_groups_smiles = {
+    'Alcohol': 'CO',
+    'Aldehyde': 'CC=O',
+    'Ketone': 'CC(=O)C',
+    'Carboxylic Acid': 'CC(=O)O',
+    'Ester': 'CC(=O)OC',
+    'Ether': 'CCOCC',
+    'Amide': 'CC(=O)N',
+    'Amine': 'CN',
+    'Nitrile': 'CC#N',
+    'Chloride': 'CCl',
+    'Bromide': 'CBr',
+    'Fluoride': 'CF',
+    'Iodide': 'CI',
+    'Alkene': 'C=C',
+    'Alkyne': 'C#C',
+    'Imine': 'C=NC',
+    'Amino acid': 'CC(N)C(=O)O',
+    'Thiol': 'CS',
+    'Epoxyde': '[*]C1([*])OC1([*])[*]',
+    'Sulfides': 'CSC',
+    'Acyl Chloride': 'CC(=O)Cl',
+    'Anhydride': 'CC(=O)OC(=O)C',
+    'Nitro': 'C[N+](=O)[O-]',
+    'Enamine': 'C=CN',
+    'Imide': 'C(=O)NC(=O)C',
+    'Azide': 'CNNN',
+    'Enol': 'C=C(O)C',
+    'Hemiacetal': 'CC(O)C',
+    'Carbonate': 'OC(=O)O',
+    'Disulfide': 'CSSC',
+    'Sulfoxide': 'CS(=O)C',
+    'Sulfone': 'CS(=O)(=O)C',
+    'Sulfonic acid': 'CS(=O)(=O)O',
+    'Sulfanote ester': 'CS(=O)(=O)OC',
+    'Thioester': 'C(=O)SC',
+    'Phosphine': 'CP',
+    'Phosphate ester': 'COP(=O)(O)O'
+}
 
-    list_contained_subgroups, list_contained_subgroups_values = [],[]
+def subgroup_value(mol_smi, dict_functional_groups):
+    list_contained_subgroups, list_contained_subgroups_values = [], []
     mol = Chem.MolFromSmiles(mol_smi)
 
     for SMILES, value in dict_functional_groups.items():
-        substruct = Chem.MolFromSmiles(SMILES)
+        substruct = Chem.MolFromSmiles(dict_functional_groups[SMILES])  # Get the SMILES string from the dictionary
 
         if mol.HasSubstructMatch(substruct):
-
             list_contained_subgroups.append(SMILES)
             list_contained_subgroups_values.append(value)
             indices = mol.GetSubstructMatch(substruct)
             print(indices)
-
         else:
-
             list_contained_subgroups.append(f'NOT {SMILES}')
 
-    return list_contained_subgroups,list_contained_subgroups_values
+    if 'Aldehyde' and 'Ketone' in list_contained_subgroups:
+        mol_aldehyde = Chem.MolFromSmiles('CC=O')
+        mol_ketone = Chem.MolFromSmiles('C(=O)C')
+
+        list_aldehyde_index = mol.GetSubstructMatch(mol_aldehyde)
+        list_ketone_index = mol.GetSubstructMatch(mol_ketone)
+
+        for x in list_aldehyde_index:
+            if x in list_ketone_index:
+                list_contained_subgroups[1] = 'NOT aldehyde'
+
+    if 'Carboxylic acid' and 'Aldehyde' and 'Alcohol' in list_contained_subgroups:
+
+        mol_acid = Chem.MolFromSmiles('CC(=O)O')
+        mol_aldehyde = Chem.MolFromSmiles('CC=O')
+        mol_alcohol = Chem.MolFromSmiles('CO')
+
+        list_aldehyde_index = mol.GetSubstructMatch(mol_aldehyde)
+        list_acid_index = mol.GetSubstructMatch(mol_acid)
+        list_alcohol_index = mol.GetSubstructMatch(mol_alcohol)
+
+        for x in list_acid_index:
+            if x in list_aldehyde_index:
+                list_contained_subgroups[1] = 'NOT Aldehyde'
+            if x in list_alcohol_index:
+                list_contained_subgroups[0] = 'NOT Alcohol'
+
+    if 'Carboxylic acid' and 'Ester' and 'Ether' in list_contained_subgroups:
+
+        mol_acid = Chem.MolFromSmiles('CC(=O)O')
+        mol_ester = Chem.MolFromSmiles('CC(=O)OC')
+        mol_ether = Chem.MolFromSmiles('COC')
+
+        list_acid_index = mol.GetSubstructMatch(mol_acid)
+        list_ester_index = mol.GetSubstructMatch(mol_ester)
+        list_ether_index = mol.GetSubstructMatch(mol_ether)
+
+        for x in list_ester_index:
+            if x in list_acid_index:
+                list_contained_subgroups[3] = 'NOT Carboxylic Acid'
+            if x in list_ether_index:
+                list_contained_subgroups[5] = 'NOT Ether'
+
+    if 'Ether' and 'Alcohol' in list_contained_subgroups:
+        mol_alcohol = Chem.MolFromSmiles('CO')
+        mol_ether = Chem.MolFromSmiles('COC')
+
+        list_alcohol_index = mol.GetSubstructMatch(mol_alcohol)
+        list_ether_index = mol.GetSubstructMatch(mol_ether)
 
 
+    return list_contained_subgroups, list_contained_subgroups_values
+
+
+print(subgroup_value(input_mol,functional_groups_smiles))
+
+
+#alcohol ok
+#aldehyde ok
+#ketone ok
