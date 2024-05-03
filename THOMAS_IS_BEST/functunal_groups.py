ab4b367e3753b56c0e0748adf29a2ee82bc76e7d
Thomas Viking Christiansson <160872481+ThomasCsson@users.noreply.github.com>
2024-05-03 08:09:46 +0200

    Update

---
+++ THOMAS_IS_BEST/functunal_groups.py
@@ -0,0 +1,49 @@
+from rdkit import Chem
+
+functional_groups_smiles = {
+    'Alcohol': 'CO',
+    'Aldehyde': 'CC=O',
+    'Ketone': 'CC(=O)C',
+    'Carboxylic Acid': 'CC(=O)O',
+    'Ester': 'CC(=O)OC',
+    'Ether': 'COC',
+    'Amide': 'CC(=O)N',
+    'Amine': 'CN',
+    'Chloride': 'CCl',
+    'Bromide': 'CBr',
+    'Fluoride': 'CF',
+    'Iodide': 'CI',
+    'Alkene': 'C=C',
+    'Alkyne': 'C#C',
+    'Imine': 'C=NC',
+    'Amino acid': 'CC(N)C(=O)O',
+    'Thiol': 'CS',
+    'Sulfides': 'CSC',
+    'Acyl Chloride': 'CC(=O)Cl',
+    'Anhydride': 'CC(=O)OC(=O)C',
+    'Nitro': 'C[N+](=O)[O-]',
+    'Enamine': 'C=CN',
+    'Imide': 'C(=O)NC(=O)C',
+    'Azide': 'CNNN',
+    'Enol': 'C=C(O)C',
+    'Hemiacetal': 'CC(O)(O)C',
+    'Carbonate': 'OC(=O)O',
+    'Disulfide': 'CSSC',
+    'Sulfoxide': 'CS(=O)C',
+    'Sulfone': 'CS(=O)(=O)C',
+    'Sulfonic  acid': 'CS(=O)(=O)O',
+    'Thioester': 'C(=O)SC',
+    'Phosphine': 'CP',
+    'Phosphate': 'COP(=O)(O)O',
+    'Benzene': 'C1=CC=CC=C1'
+}
+
+
+mol_smi = input('SMILEs: ')
+mol = Chem.MolFromSmiles(mol_smi)
+
+pattern = 'C=O'
+pat = Chem.MolFromSmiles(pattern)
+
+list = (mol.GetSubstructMatch(pat))
+print(list[0])
