5ec559622b653837c1b29381e784f3f7e0f0f650
Thomas Viking Christiansson <160872481+ThomasCsson@users.noreply.github.com>
2024-05-04 12:43:54 +0200

    Update

---
+++ THOMAS_IS_BEST/__pycache__/test3.py
@@ -0,0 +1,110 @@
+from rdkit import Chem
+from rdkit.Chem import Draw
+def functional_group_finder(mol_smi):
+
+    mol_in = Chem.MolFromSmiles(mol_smi)
+    functional_groups_smarts = {
+        'Alcohol': 'C[Oh1+0]',
+        'Aldehyde': 'C[Ch1]=O',
+        'Ketone': 'CC(=O)C',
+        'Carboxylic Acid': 'CC(=O)[Oh1]',
+        'Ester': 'CC(=O)[Oh0]',
+        'Ether': '*[Oh0]*',
+        'Amide': 'C(=O)N',
+        'Amine': '[C][Nh2]',
+        'Nitrile': 'C#N',
+        'Chloride': 'Cl',
+        'Bromide': 'Br',
+        'Fluoride': 'F',
+        'Iodide': 'I',
+        'Alkene': 'C=C',
+        'Alkyne': 'C#C',
+        'Imine': 'C=N*',
+        'Amino acid': '[Nh2][Ch1*]C(=O)O',
+        'Proline': '[Nh1][Ch1*]C(=O)O',
+        'Thiol': '[Sh1]',
+        'Sulfide': '*[Sh0]*',
+        'Acyl Chloride': 'CC(=O)Cl',
+        'Anhydride': '*[Ch0](=O)O[Ch0](=O)*',
+        'Nitro': 'C[N+](=O)[O-]',
+        'Enamine': 'C=C[Nh0]',
+        'Enamine2': 'C=C[Nh1]',
+        'Enamine3': 'C=C[Nh2]',
+        'Imide': 'C(=O)NC(=O)*',
+        'Azide': 'CNNN',
+        'Enol': 'C=C([Oh1])C',
+        'Hemiacetal': 'CC(O)(O)C',
+        'Carbonate': '[Oh0]C(=O)[Oh0]',
+        'Carbonate2': '[Oh1]C(=O)[Oh1]',
+        'Disulfide': 'CSSC',
+        'Sulfoxide': 'CS(=O)C',
+        'Sulfone': '*[So2](=O)(=O)*',
+        'Sulfonic acid': '*S(=O)(=O)[Oh1]',
+        'Thioester': 'C(=O)S*',
+        'Phosphine': '*[Po0](*)*',
+        'Phosphate': '*OP(=O)(O)O',
+        'Benzene': 'c1ccccc1'
+    }
+
+    '''mol = Chem.MolFromSmarts('C[Ch1]=O')
+    mol1 = Chem.MolFromSmiles('CCC(=O)C')
+    print(mol1.HasSubstructMatch(mol))'''
+
+    
+    
+    functional_groups_contained = []
+
+    for name, smarts in functional_groups_smarts.items():
+        functional_group = Chem.MolFromSmarts(smarts)
+        if mol_in.HasSubstructMatch(functional_group):
+            for _ in range(len(mol_in.GetSubstructMatches(functional_group))):
+                functional_groups_contained.append(name)
+            
+    print(functional_groups_contained)
+
+    if 'Carboxylic Acid' in functional_groups_contained:
+        functional_groups_contained.remove('Alcohol')
+    if 'Ester' in functional_groups_contained:
+        functional_groups_contained.remove('Ether')
+    if 'Phosphate' in functional_groups_contained:
+        functional_groups_contained.remove('Ether')
+    if 'Thioester' in functional_groups_contained:
+        functional_groups_contained.remove('Sulfide')
+    if 'Sulfonic acid' in functional_groups_contained:
+        functional_groups_contained.remove('Sulfide')
+    if 'Sulfoxide' in functional_groups_contained:
+        functional_groups_contained.remove('Sulfide')
+    if 'Acyl Chloride' in functional_groups_contained:
+        functional_groups_contained.remove('Chloride')
+    if 'Anhydride' in functional_groups_contained:
+        functional_groups_contained.remove('Ester')
+        functional_groups_contained.remove('Ester')
+    if 'Enamine2' in functional_groups_contained:
+        functional_groups_contained.remove('Enamine2')
+        functional_groups_contained.append('Enamine')
+    if 'Enamine3' in functional_groups_contained:
+        functional_groups_contained.remove('Enamine3')
+        functional_groups_contained.remove('Amine')
+        functional_groups_contained.append('Enamine')
+    if 'Imide' in functional_groups_contained:
+        functional_groups_contained.remove('Amide')
+        functional_groups_contained.remove('Amide')
+    if 'Enol' in functional_groups_contained:
+        functional_groups_contained.remove('Alkene')
+        functional_groups_contained.remove('Alcohol')
+    if 'Hemiacetal' in functional_groups_contained:
+        functional_groups_contained.remove('Alcohol')
+        functional_groups_contained.remove('Alcohol')
+    if 'Carbonate2' in functional_groups_contained:
+        functional_groups_contained.remove('Alcohol')
+        functional_groups_contained.remove('Alcohol')
+        functional_groups_contained.remove('Carbonate2')
+        functional_groups_contained.append('Carbonate')
+    if 'Disulfide' in functional_groups_contained:
+        functional_groups_contained.remove('Sulfide')
+        functional_groups_contained.remove('Sulfide')
+    return functional_groups_contained
+
+
+mol_smi = input('SMILEs: ')
+print(functional_group_finder(mol_smi))
--- THOMAS_IS_BEST/functunal_groups.py
+++ THOMAS_IS_BEST/functunal_groups.py
@@ -1,55 +1,5 @@
 from rdkit import Chem
 
-
-functional_groups_smarts = {
-    'Alcohol': 'C[Oh1]',
-    'Aldehyde': 'C[Ch1]=O',
-    'Ketone': 'CC(=O)C',
-    'Carboxylic Acid': 'CC(=O)[Oh1]',
-    'Ester': 'CC(=O)OC',
-    'Ether': 'COC',
-    'Amide': 'CC(=O)N',
-    'Amine': 'C[Nh2]',
-    'Nitrile': 'C#N',
-    'Chloride': 'CCl',
-    'Bromide': 'CBr',
-    'Fluoride': 'CF',
-    'Iodide': 'CI',
-    'Alkene': 'C=C',
-    'Alkyne': 'C#C',
-    'Imine': 'C=NC',
-    'Amino acid': 'CC(N)C(=O)O',
-    'Thiol': 'C[Sh1]',
-    'Sulfides': 'CSC',
-    'Acyl Chloride': 'CC(=O)Cl',
-    'Anhydride': 'CC(=O)OC(=O)C',
-    'Nitro': 'C[N+](=O)[O-]',
-    'Enamine': 'C=CN',
-    'Imide': 'C(=O)NC(=O)C',
-    'Azide': 'CNNN',
-    'Enol': 'C=C([Oh1])C',
-    'Hemiacetal': 'CC(O)(O)C',
-    'Carbonate': 'OC(=O)O',
-    'Disulfide': 'CSSC',
-    'Sulfoxide': 'CS(=O)C',
-    'Sulfone': 'CS(=O)(=O)C',
-    'Sulfonic  acid': 'CS(=O)(=O)O',
-    'Thioester': 'C(=O)SC',
-    'Phosphine': 'CP',
-    'Phosphate': 'COP(=O)(O)O',
-    'Benzene': 'C1=CC=CC=C1'
-}
-
-mol = Chem.MolFromSmarts('CCO')
-pat = Chem.MolFromSmarts('C[Oh1]')
-print(mol.HasSubstructMatch(pat))
-
-
-'''for name, smarts in functional_groups_smarts.items():
-    for name2, smarts2 in functional_groups_smarts.items():
-        mol = Chem.MolFromSmarts(smarts)
-        mol2 = Chem.MolFromSmarts(smarts2)
-        if mol.HasSubstructMatch(mol2):
-            print(name, name2)'''
-            
-    
+mol = Chem.MolFromSmarts('*[Sh0]*')
+mol1 = Chem.MolFromSmiles('CSC')
+print(mol1.HasSubstructMatch(mol))
