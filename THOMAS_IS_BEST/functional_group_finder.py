80683ae8a38c75c106f0fbf53f5034837633c593
Thomas Viking Christiansson <160872481+ThomasCsson@users.noreply.github.com>
2024-05-04 12:59:28 +0200

    Update functional_group_finder.py

--- THOMAS_IS_BEST/functional_group_finder.py
+++ THOMAS_IS_BEST/functional_group_finder.py
@@ -1,5 +1,4 @@
 from rdkit import Chem
-from rdkit.Chem import Draw
 def functional_group_finder(mol_smi):
 
     #---------------------------------------------------------------------------------------------#
