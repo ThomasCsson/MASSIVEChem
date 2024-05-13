5d99d270ae1f7493b3d01f91a32467b21ade6d60
Igor Gonteri <igor.gonteri@epfl.ch>
2024-05-13 14:32:46 +0200

    z

--- Bokeh/layout.py
+++ Bokeh/layout.py
@@ -1,25 +1,6 @@
-#Imports
-
 from rdkit import Chem
-from rdkit.Chem import Draw, AllChem
-
-import time
-import pandas as pd
-
-import numpy as np
-
-import matplotlib.pyplot as plt
-
-import plotly.graph_objects as go
-from plotly.subplots import make_subplots
-
-from bokeh.plotting import figure, show, row
-from bokeh.models.tickers import FixedTicker
-from bokeh.layouts import row
-from bokeh.io import show
-from bokeh.models import ColumnDataSource, HTMLTemplateFormatter, WheelPanTool, WheelZoomTool, BoxAnnotation, CustomJS
-from bokeh.models.widgets import DataTable, TableColumn
-from bokeh.layouts import column
+from rdkit.Chem import AllChem
+from rdkit.Chem import Draw
 
 def save_molecule_image_to_file(mol_smi, file_path, show_Hs=False, show_3D = False):
 
@@ -55,56 +36,9 @@ def save_molecule_image_to_file(mol_smi, file_path, show_Hs=False, show_3D = Fal
     # Save the image to a file
     image.save(file_path)
 
+filepath = 'Bokeh/molecule_image.png'
+mol = 'CCCCC'
+
+save_molecule_image_to_file(mol, filepath)
 
 
-
-
-
-def mol_web_show(image_url):
-
-    #---------------------------------------------------------------------------------------------#
-    '''
-    mol_web_show(image_url)
-    
-    Input: path of the molecule image
-    
-    Output: image of the molecule in bokeh
-
-    '''
-    #---------------------------------------------------------------------------------------------#
-
-
-    # Creating a Bokeh figure to display the molecule
-    p = figure(width=400, height=400,toolbar_location=None, x_range=(0, 1), y_range=(0, 1))
-    p.image_url(url=[image_url], x=0, y=1, w=1, h=1)
-
-    # Hide grid lines and axes
-    p.xgrid.grid_line_color = None
-    p.ygrid.grid_line_color = None
-    p.xaxis.visible = False
-    p.yaxis.visible = False
-
-    show(p)
-
-    return p
-
-    
-
-mol_smi = input('SMI  ')
-
-# Define the output file path
-output_file_path = "molecule_image.png"
-
-# Generate and save the molecule image
-save_molecule_image_to_file(mol_smi, output_file_path, show_Hs=False, show_3D=False)
-
-# Display the molecule image using Bokeh
-mol_web_show(output_file_path)
-
-
-
-
-def all_in_one(p1,p2,p3):
-    layout1 = row(p2, p3)
-    layout = column(p1, layout1)
-    return layout
