35486f864b173e67d5fb48a598871689924e1b9b
Igor Gonteri <igor.gonteri@epfl.ch>
2024-05-13 11:38:48 +0200

    p

--- THOMAS_IS_BEST/functions.py
+++ THOMAS_IS_BEST/functions.py
@@ -374,7 +374,7 @@ def delta_function_plotter(x_in, y_in):
 
     min_x , max_x = min(x_in), max(x_in)
 
-    x_axis, y_axis = [min_x-1],[0]
+    x_axis, y_axis = [min_x-0.5],[0]
     for i in range (len(x_in)):
         x_axis.append(x_in[i]-10**(-10))
         x_axis.append(x_in[i])
@@ -728,78 +728,9 @@ def functional_group_finder(mol_smi):
     return functional_groups_contained
 
 
-def double_plot(x_in,y_in):
-    #---------------------------------------------------------------------------------------------#
-    '''
-    double_plot(x_in,y_in)
-    
-    Input: list of masses (x_in) and intensities (y_in)
-    
-    Output: 2 Bokeh graphs:
-            - One that shows the mass spectrum of the molecule to which the user can interact
-            -Another that is the same graph but shows where the user is zooming on the first graph
-    '''
-    #---------------------------------------------------------------------------------------------#
-
-    # tells where to put the graduation on the graph
-
-    ticked_peaks = []
-    for i in range(len(x_in)):
-        if y_in[i] > 0.0001:
-            ticked_peaks.append(x_in[i])
-
-    #creates the principal graph, mass spectrum of the molecule (interactive)
-
-    p1 = figure(width=700, title=f'Mass spectrum of molecule')
-    p1 = figure(x_axis_label = '[m/z]')
-    p1 = figure(y_axis_label = 'Abundance')
-    p1.xaxis.axis_label = "[m/z]"
-    p1.height = 500
-    p1.xaxis.ticker = FixedTicker(ticks=ticked_peaks)
-    p1.add_tools(WheelPanTool(dimension="height"))
-    p1.add_tools(WheelZoomTool(dimensions="height"))
-    p1.line(x_in, y_in, line_width=1)
-    p1.xaxis.major_label_orientation = "horizontal"
-
-     #creates the secondary graph, mass spectrum of the molecule (non-interactive)
-
-    p2 = figure(title="Simulated Mass Spectrum", x_axis_label='Mass [Th]', y_axis_label='Intensity')
-    p2 = figure(width=300, title=f'Mass spectrum of molecule')
-    p2 = figure(toolbar_location=None)
-    p2.height = 300
-    p2.line(x_in, y_in, legend_label="Mass spectrum", line_width=1)
-
-    #creates a tool in order that the second graph shows where the zoom is on the first one
-
-    box = BoxAnnotation(left=0, right=0, bottom=0, top=0,
-    fill_alpha=0.1, line_color='red', fill_color='cornflowerblue')
-
-    jscode = """
-        box[%r] = cb_obj.start
-        box[%r] = cb_obj.end
-    """
-
-    xcb = CustomJS(args=dict(box=box), code=jscode % ('left', 'right'))
-    ycb = CustomJS(args=dict(box=box), code=jscode % ('bottom', 'top'))
-
-    p1.x_range.js_on_change('start', xcb)
-    p1.x_range.js_on_change('end', xcb)
-    p1.y_range.js_on_change('start', ycb)
-    p1.y_range.js_on_change('end', ycb)
-
-    # adds the functionnality to the second figure
-
-    p2.add_layout(box)
-
-    # creates a layout that displays the 2 graphs
-
-    layout = row(p1, p2)
-    show(layout)
-
-    return layout
-
 
 def functional_group_display(groups_list):
+
     #---------------------------------------------------------------------------------------------#
     '''
     functional_group_display(groups_list)
@@ -885,7 +816,81 @@ def functional_group_display(groups_list):
 
     data_table = DataTable(source=source, columns=columns, width=250, height=table_height, row_height=60)
 
+    show(data_table)
+
     return data_table
+
+
+def double_plot(x_in,y_in):
+
+    #---------------------------------------------------------------------------------------------#
+    '''
+    double_plot(x_in,y_in)
+    
+    Input: list of masses (x_in) and intensities (y_in)
+    
+    Output: 2 Bokeh graphs:
+            - One that shows the mass spectrum of the molecule to which the user can interact
+            -Another that is the same graph but shows where the user is zooming on the first graph
+    '''
+    #---------------------------------------------------------------------------------------------#
+
+    # tells where to put the graduation on the graph
+
+    ticked_peaks = []
+    for i in range(len(x_in)):
+        if y_in[i] > 0.0001:
+            ticked_peaks.append(x_in[i])
+
+    #creates the principal graph, mass spectrum of the molecule (interactive)
+
+    p1 = figure(width=700, title=f'Mass spectrum of molecule')
+    p1 = figure(x_axis_label = '[m/z]')
+    p1 = figure(y_axis_label = 'Abundance')
+    p1.xaxis.axis_label = "[m/z]"
+    p1.height = 500
+    p1.xaxis.ticker = FixedTicker(ticks=ticked_peaks)
+    p1.add_tools(WheelPanTool(dimension="height"))
+    p1.add_tools(WheelZoomTool(dimensions="height"))
+    p1.line(x_in, y_in, line_width=1)
+    p1.xaxis.major_label_orientation = "horizontal"
+
+     #creates the secondary graph, mass spectrum of the molecule (non-interactive)
+
+    p2 = figure(title="Simulated Mass Spectrum", x_axis_label='Mass [Th]', y_axis_label='Intensity')
+    p2 = figure(width=300, title=f'Mass spectrum of molecule')
+    p2 = figure(toolbar_location=None)
+    p2.height = 300
+    p2.line(x_in, y_in, legend_label="Mass spectrum", line_width=1)
+
+    #creates a tool in order that the second graph shows where the zoom is on the first one
+
+    box = BoxAnnotation(left=0, right=0, bottom=0, top=0,
+    fill_alpha=0.1, line_color='red', fill_color='cornflowerblue')
+
+    jscode = """
+        box[%r] = cb_obj.start
+        box[%r] = cb_obj.end
+    """
+
+    xcb = CustomJS(args=dict(box=box), code=jscode % ('left', 'right'))
+    ycb = CustomJS(args=dict(box=box), code=jscode % ('bottom', 'top'))
+
+    p1.x_range.js_on_change('start', xcb)
+    p1.x_range.js_on_change('end', xcb)
+    p1.y_range.js_on_change('start', ycb)
+    p1.y_range.js_on_change('end', ycb)
+
+    # adds the functionnality to the second figure
+
+    p2.add_layout(box)
+
+    # creates a layout that displays the 2 graphs
+
+    layout = row(p1, p2)
+    show(layout)
+
+    return layout
 
 
 def save_molecule_image_to_file(mol_smi, file_path, show_Hs=False, show_3D = False):
