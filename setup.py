d5de1ff53b5b97a558eb55ae997e674b820e5fc3
Arthur Humery <160743818+Arthurhmy@users.noreply.github.com>
2024-05-13 11:31:34 +0200

    update

--- setup.py
+++
@@ -1,6 +0,0 @@
-from setuptools import setup, find_packages
-
-with open("requirements.txt", "r") as f:
-    requirements = f.read().splitlines()
-
-setup(name="ChemInterface", version='1.0', packages=find_packages(), install_requires=requirements,)
