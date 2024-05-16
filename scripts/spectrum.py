e4909665d8a88633f2c76ea6690b7789df2fa2df
Thomas Viking Christiansson <160872481+ThomasCsson@users.noreply.github.com>
2024-05-16 15:32:17 +0200

    Update

--- scripts/spectrum.py
+++ scripts/spectrum.py
@@ -17,7 +17,7 @@ def spectrum(mol_smi, imprecision_True_False, apparatus_resolution,search_direct
     '''
     spectrum(mol_smi, imprecision_True_False, apparatus_resolution)
     
-    Input: molecule under SMILEs representation
+    Input: molecule under SMILEs representation (string), True or False (True gives a less precise spectrum, but is actually able to run in adequate time), apparatus resolution (typically, a value of 0.01 is used here)
     
     Output: webpage with mass spectrum and images relating to molecule
     '''
@@ -581,4 +581,5 @@ def spectrum(mol_smi, imprecision_True_False, apparatus_resolution,search_direct
     final = row(layout, last)
     return final
 
-show(spectrum('CCO',True,0.001))
+show(spectrum('CCO',True,0.01))
+show(spectrum())
