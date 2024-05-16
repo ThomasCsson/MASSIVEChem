d20141d1cba5a72880bd962a4edae1a90f7ddfc4
Arthur Humery <160743818+Arthurhmy@users.noreply.github.com>
2024-05-16 14:18:39 +0200

    Update insaturation.py

--- scripts/insaturation.py
+++ scripts/insaturation.py
@@ -2,10 +2,15 @@ from rdkit import Chem
 from rdkit.Chem import Draw
 
 def calculate_insaturation(mol_smile) -> int:
-    # This function takes as input a string representing the SMILES notation of a molecule. 
-    # Using the RDKit library, it creates a representation of the molecule, adds hydrogens and then calculates the unsaturation level of the molecule. 
-    # The level of unsaturation is determined by counting the number of carbon, nitrogen and halogen atoms, then applying a specific formula. 
-    # Finally, the function displays the unsaturation level and the image of the molecule, and indicates the time required to perform the function.
+    #---------------------------------------------------------------------------------------------#
+    '''
+    This function takes as input a string representing the SMILES notation of a molecule. 
+    Using the RDKit library, it creates a representation of the molecule, adds hydrogens and then calculates the unsaturation level of the molecule. 
+    The level of unsaturation is determined by counting the number of carbon, nitrogen and halogen atoms, then applying a specific formula. 
+    Finally, the function displays the unsaturation level and the image of the molecule, and indicates the time required to perform the function.
+    '''
+    #---------------------------------------------------------------------------------------------#
+
     mol_1 = Chem.MolFromSmiles(mol_smile)
     mol = Chem.AddHs(mol_1)
 
