40e4607ebf921b41493613e460f77910c1272380
Thomas Viking Christiansson <160872481+ThomasCsson@users.noreply.github.com>
2024-05-17 11:49:44 +0200

    Update

--- THOMAS_IS_BEST/test.py
+++ THOMAS_IS_BEST/test.py
@@ -1,23 +1,125 @@
-x = [1,1.2,2,2.3,4,4.1,4.2]
-y = [0,0,0,0,0,0,0]
+from rdkit import Chem
 
+def functional_group_finder(mol_smi):
 
-def peak_merger(x_in, y_in):
-    x_out, y_out = [],[]
-    while len(x_in)>1:
-        if x_in[0]>x_in[1]-0.2:
-            y_in[1] = y_in[0] + y_in[1]
-            x_in[1] = (x_in[0] + x_in[1])/2
-            x_in.pop(0)
-            y_in.pop(0)
-        else:
-            x_out.append(x_in[0])
-            y_out.append(y_in[0])
-            x_in.pop(0)
-            y_in.pop(0)
-    x_out.append(x_in[0])
-    y_out.append(y_in[0])
+    #---------------------------------------------------------------------------------------------#
+    '''
+    functional_group_finder(mol_smi)
+    
+    Input: molecule under SMILEs representation
+    
+    Output: list containing every functionl group contained (if a functional group is contained twice in the molecule, it will appear twice in this list)
+    '''
+    #---------------------------------------------------------------------------------------------#
 
-    return x_out, y_out
+    # initiate variables
+    functional_groups_contained, mol_in = [], Chem.MolFromSmiles(mol_smi)
 
-print(peak_merger(x,y))
+    # dictionnary of all the considered functional groups to check (some might be missing)
+
+    functional_groups_smarts = {
+        'Alcohol': 'C[Oh1+0]',
+        'Aldehyde': 'C[Ch1]=O',
+        'Ketone': 'CC(=O)C',
+        'Carboxylic Acid': 'CC(=O)[Oh1]',
+        'Ester': 'CC(=O)[Oh0]',
+        'Ether': '*[Oh0]*',
+        'Amide': 'C(=O)N',
+        'Amine': '[C][N]',
+        'Nitrile': 'C#N',
+        'Chloride': 'Cl',
+        'Bromide': 'Br',
+        'Fluoride': 'F',
+        'Iodide': 'I',
+        'Alkene': 'C=C',
+        'Alkyne': 'C#C',
+        'Imine': 'C=N*',
+        'Amino acid': '[Nh2][Ch1*]C(=O)O',
+        'Proline': '[Nh1][Ch1*]C(=O)O',
+        'Thiol': '[Sh1]',
+        'Sulfide': '*[Sh0]*',
+        'Acyl Chloride': 'CC(=O)Cl',
+        'Anhydride': '*[Ch0](=O)O[Ch0](=O)*',
+        'Nitro': 'C[N+](=O)[O-]',
+        'Enamine': 'C=C[Nh0]',
+        'Enamine2': 'C=C[Nh1]',
+        'Enamine3': 'C=C[Nh2]',
+        'Imide': 'C(=O)NC(=O)*',
+        'Azide': 'CNNN',
+        'Enol': 'C=C([Oh1])C',
+        'Hemiacetal': 'CC(O)(O)C',
+        'Carbonate': '[Oh0]C(=O)[Oh0]',
+        'Carbonate2': '[Oh1]C(=O)[Oh1]',
+        'Disulfide': 'CSSC',
+        'Sulfoxide': 'CS(=O)C',
+        'Sulfone': '*[So2](=O)(=O)*',
+        'Sulfonic acid': '*S(=O)(=O)[Oh1]',
+        'Thioester': 'C(=O)S*',
+        'Phosphine': '*[Po0](*)*',
+        'Phosphate': '*OP(=O)(O)O',
+        'Benzene': 'c1ccccc1',
+        'Peroxide':'C[Oh0][Oh0]C'
+    }
+
+    # check that the substructure from functional_groups_smarts are contained in mol_smi
+
+    for name, smarts in functional_groups_smarts.items():
+        if mol_in.HasSubstructMatch(Chem.MolFromSmarts(smarts)):
+            for _ in range(len(mol_in.GetSubstructMatches(Chem.MolFromSmarts(smarts)))):
+                functional_groups_contained.append(name)
+
+    # exceptions for conflicts during the iteration of functional groups
+    for functional_group in functional_groups_contained:
+        if functional_group == 'Carboxylic Acid':
+            functional_groups_contained.remove('Alcohol')
+        elif 'Ester' == functional_group:
+            functional_groups_contained.remove('Ether')
+        elif 'Phosphate' == functional_group:
+            functional_groups_contained.remove('Ether')
+        elif 'Thioester' == functional_group:
+            functional_groups_contained.remove('Sulfide')
+        elif 'Sulfonic acid' == functional_group:
+            functional_groups_contained.remove('Sulfide')
+        elif 'Sulfoxide' == functional_group:
+            functional_groups_contained.remove('Sulfide')
+        elif 'Acyl Chloride' == functional_group:
+            functional_groups_contained.remove('Chloride')
+        elif 'Anhydride' == functional_group:
+            functional_groups_contained.remove('Ester')
+            functional_groups_contained.remove('Ester')
+        elif 'Enamine2' == functional_group:
+            functional_groups_contained.remove('Enamine2')
+            functional_groups_contained.append('Enamine')
+        elif 'Enamine3' == functional_group:
+            functional_groups_contained.remove('Enamine3')
+            functional_groups_contained.remove('Amine')
+            functional_groups_contained.append('Enamine')
+        elif 'Imide' == functional_group:
+            functional_groups_contained.remove('Amide')
+            functional_groups_contained.remove('Amide')
+        elif 'Enol' == functional_group:
+            functional_groups_contained.remove('Alkene')
+            functional_groups_contained.remove('Alcohol')
+        elif 'Hemiacetal' == functional_group:
+            functional_groups_contained.remove('Alcohol')
+            functional_groups_contained.remove('Alcohol')
+        elif 'Carbonate2' == functional_group:
+            functional_groups_contained.remove('Alcohol')
+            functional_groups_contained.remove('Alcohol')
+            functional_groups_contained.remove('Carbonate2')
+            functional_groups_contained.append('Carbonate')
+        elif 'Disulfide' == functional_group:
+            functional_groups_contained.remove('Sulfide')
+            functional_groups_contained.remove('Sulfide')
+        elif 'Amine' == functional_group:
+            functional_groups_contained.remove('Amine')
+        elif 'Peroxide' == functional_group:
+            functional_groups_contained.remove('Ether')
+            functional_groups_contained.remove('Ether')
+        elif 'Amide' == functional_group:
+            functional_groups_contained.remove('Amine')
+    
+    
+    return functional_groups_contained
+
+print(functional_group_finder('CC1(C(N2C(S1)C(C2=O)NC(=O)CC3=CC=CC=C3)C(=O)O)C'))
