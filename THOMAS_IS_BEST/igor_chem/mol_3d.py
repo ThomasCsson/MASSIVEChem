bbf7fbf0b4609e2330cc33cfbcf012aaffbd7173
Igor Gonteri <igor.gonteri@epfl.ch>
2024-05-20 19:44:06 +0200

    e

--- THOMAS_IS_BEST/igor_chem/mol_3d.py
+++ THOMAS_IS_BEST/igor_chem/mol_3d.py
@@ -44,6 +44,50 @@ fig = to_plotly_figure(mg)
 import panel as pn
 
 plotly_pane = pn.pane.Plotly(fig)
-plotly_pane.show
+
+
+from math import pi
+
+from bokeh.palettes import Category20c, Category20
+from bokeh.plotting import figure
+from bokeh.transform import cumsum
+
+x = {
+    'United States': 157,
+    'United Kingdom': 93,
+    'Japan': 89,
+    'China': 63,
+    'Germany': 44,
+    'India': 42,
+    'Italy': 40,
+    'Australia': 35,
+    'Brazil': 32,
+    'France': 31,
+    'Taiwan': 31,
+    'Spain': 29
+}
+
+data = pd.Series(x).reset_index(name='value').rename(columns={'index':'country'})
+data['angle'] = data['value']/data['value'].sum() * 2*pi
+data['color'] = Category20c[len(x)]
+
+p = figure(height=350, title="Pie Chart", toolbar_location=None,
+           tools="hover", tooltips="@country: @value", x_range=(-0.5, 1.0))
+
+r = p.wedge(x=0, y=1, radius=0.4,
+        start_angle=cumsum('angle', include_zero=True), end_angle=cumsum('angle'),
+        line_color="white", fill_color='color', legend_field='country', source=data)
+
+p.axis.axis_label=None
+p.axis.visible=False
+p.grid.grid_line_color = None
+
+bokeh_pane = pn.pane.Bokeh(p, theme="dark_minimal")
+
+
+
+final = pn.Row(bokeh_pane,plotly_pane)
+
+final.show()
 
 
