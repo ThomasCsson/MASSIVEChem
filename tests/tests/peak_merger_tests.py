774cc80ac407938212679a80cc7e8c4ba7da07e9
Igor Gonteri <igor.gonteri@epfl.ch>
2024-05-23 14:54:28 +0200

    update failed tests

--- tests/tests/peak_merger_tests.py
+++ tests/tests/peak_merger_tests.py
@@ -18,7 +18,7 @@ def peak_merger(x_in, y_in, apparatus_resolution) -> list[float]:
 
     return x_out, y_out
 
-print(peak_merger([1.0, 1.1, 2.0],[0.5, 0.5, 1.0],0.15))
+#print(peak_merger([1.0, 1.1, 2.0],[0.5, 0.5, 1.0],0.15))
 
 print(peak_merger([1.0, 2.0, 3.0],[0.5, 0.5, 1.0],0.5))
 print(peak_merger([1.0, 1.1, 1.2],[0.5, 0.3, 0.2],0.15))
