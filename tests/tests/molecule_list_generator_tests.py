115f2ee605b33c27e3dcad24ee0d3961bf4b3344
Igor Gonteri <igor.gonteri@epfl.ch>
2024-05-23 14:35:05 +0200

    update tests

--- tests/tests/molecule_list_generator_tests.py
+++ tests/tests/molecule_list_generator_tests.py
@@ -1,15 +1,27 @@
-def molecule_list_generator(mol) -> list[str]:
-    #---------------------------------------------------------------------------------------------#
-    '''
-    molecule_list_generator(mol)
-    
-    Input: molecule under MOL representation
-    
-    Output: list containing the atomic symbol of each atom in the input molecule
-    '''
-    #---------------------------------------------------------------------------------------------#
+import unittest
+from rdkit import Chem
 
+def molecule_list_generator(mol) -> list[str]:
     list_atoms = []
     for atom in mol.GetAtoms():
         list_atoms.append(atom.GetSymbol())
     return list_atoms
+
+class TestMoleculeListGenerator(unittest.TestCase):
+
+    def test_benzene(self):
+        mol_smi = "C1=CC=CC=C1"  # Benzene
+        mol = Chem.MolFromSmiles(mol_smi)
+        result = molecule_list_generator(mol)
+        expected = ['C', 'C', 'C', 'C', 'C', 'C']
+        self.assertEqual(result, expected)
+
+    def test_water(self):
+        mol_smi = "O"  # Water
+        mol = Chem.MolFromSmiles(mol_smi)
+        result = molecule_list_generator(mol)
+        expected = ['O']
+        self.assertEqual(result, expected)
+
+if __name__ == '__main__':
+    unittest.main()
