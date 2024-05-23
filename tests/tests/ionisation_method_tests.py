0f2268c3d457f545c6d64372c7a18995c5e2f8a5
Igor Gonteri <igor.gonteri@epfl.ch>
2024-05-23 13:55:23 +0200

    tr

--- tests/tests/ionisation_method_tests.py
+++ tests/tests/ionisation_method_tests.py
@@ -15,3 +15,32 @@ def ionisation_method (list_atoms) -> list[str]:
         #Check that there is in fact a proton to remove
         list_atoms.remove('H')
     return list_atoms
+
+
+import unittest
+
+class TestIonisationMethod(unittest.TestCase):
+
+    def test_remove_single_h(self):
+        self.assertEqual(ionisation_method(['H', 'C', 'O']), ['C', 'O'])
+
+    def test_remove_multiple_h(self):
+        self.assertEqual(ionisation_method(['H', 'H', 'C', 'O']), ['H', 'C', 'O'])
+
+    def test_no_h_in_list(self):
+        self.assertEqual(ionisation_method(['C', 'O', 'N']), ['C', 'O', 'N'])
+
+    def test_only_h_in_list(self):
+        self.assertEqual(ionisation_method(['H']), [])
+
+    def test_empty_list(self):
+        self.assertEqual(ionisation_method([]), [])
+
+    def test_no_removal_for_other_atoms(self):
+        self.assertEqual(ionisation_method(['C', 'O', 'N']), ['C', 'O', 'N'])
+
+    def test_remove_h_from_complex_list(self):
+        self.assertEqual(ionisation_method(['H', 'H', 'C', 'O', 'H']), ['H', 'C', 'O', 'H'])
+
+if __name__ == '__main__':
+    unittest.main()
