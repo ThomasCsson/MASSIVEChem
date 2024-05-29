c85c99d0a5902202dc07a9fff180a7d9bc201621
Igor Gonteri <igor.gonteri@epfl.ch>
2024-05-29 16:51:15 +0200

    2

--- test/test_calculate_unsaturation.py
+++ test/test_calculate_unsaturation.py
@@ -1,6 +1,6 @@
 from rdkit import Chem
-from rdkit.Chem import Draw
-
+import MASSiveChem.MASSiveChem as MC
+"""
 def calculate_unsaturation(mol_smile) -> int:
     #---------------------------------------------------------------------------------------------#
     '''
@@ -32,44 +32,44 @@ def calculate_unsaturation(mol_smile) -> int:
     unsaturation = C + 1 + (N - HX) / 2
 
     return unsaturation
-
+"""
 import unittest
 
 class TestCalculateUnsaturation(unittest.TestCase):
 
     def test_one_unsaturation(self):
 
-        result = calculate_unsaturation("C=C")  
+        result = MC.calculate_unsaturation("C=C")  
         self.assertEqual(result, 1)
 
     def test_nitrogen_and_no_halogens(self):
 
-        result = calculate_unsaturation("CC=O")  
+        result = MC.calculate_unsaturation("CC=O")  
         self.assertEqual(result, 1)
 
     def test_nitrogen_and_halogens(self):
 
-        result = calculate_unsaturation("CNC(Cl)Br")  
+        result = MC.calculate_unsaturation("CNC(Cl)Br")  
         self.assertEqual(result, 0)
 
     def test_multiple_nitrogen_and_halogens(self):
 
-        result = calculate_unsaturation("BrC=CC(Br)(Cl)CC(N)CCN") 
+        result = MC.calculate_unsaturation("BrC=CC(Br)(Cl)CC(N)CCN") 
         self.assertEqual(result, 1)
 
     def test_multiple_unsaturations(self):
 
-        result = calculate_unsaturation("C1=CC=CC=C1") 
+        result = MC.calculate_unsaturation("C1=CC=CC=C1") 
         self.assertEqual(result, 4)
 
     def test_no_atoms(self):
 
-        result = calculate_unsaturation("") 
+        result = MC.calculate_unsaturation("") 
         self.assertEqual(result, 1)
 
     def test_no_nitrogen_and_halogens(self):
 
-        result = calculate_unsaturation("CCCl")
+        result = MC.calculate_unsaturation("CCCl")
         self.assertEqual(result, 0)
 
 if __name__ == '__main__':
